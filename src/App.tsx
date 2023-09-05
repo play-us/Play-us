2b55fc1aa47ef5a75c9ea3210837c2d64c31d030
bumjin <potato980124@gamil.com>
2023-09-05 21:46:12 +0900

    feat :  헤더 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
