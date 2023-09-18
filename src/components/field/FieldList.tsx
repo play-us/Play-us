6ac319e1e523b22facc4fc8b26a7bdaf805a3a41
sunhee <tjsgml951012@gmail.com>
2023-09-19 01:17:58 +0900

    feat : #4 구장리스트 slider 추가

--- src/components/field/FieldList.tsx
+++ src/components/field/FieldList.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
 import styled from 'styled-components';
-import { Col, Row, Typography } from 'antd';
+import { Col, Row } from 'antd';
+import FieldListItem from './FieldListItem';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 interface IFieldItem {
   field_id: string;
@@ -66,132 +70,139 @@ const FieldList = () => {
     });
   }, []);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+
   return (
     <Row>
       <Col span={24}>
-        <CommunityItemWrap>
+        <FieldItemWrap>
           {!isLoading &&
             rowDataList.map((data: IRowData) => (
-              <CommunutyListItem data={data} />
+              <FieldListItem data={data} key={data.field_id} />
             ))}
-        </CommunityItemWrap>
+        </FieldItemWrap>
+        <FieldItemMoreBtn>더보기</FieldItemMoreBtn>
+        <SlideTitle>
+          주변 관심 종목 구장 리스트
+          <MoreBtn>더보기</MoreBtn>
+        </SlideTitle>
+        <SliderWrap>
+          {rowDataList.length > 0 && (
+            <Slider {...settings}>
+              {rowDataList.map((data: IRowData) => (
+                <FieldListItem data={data} key={data.field_id} />
+              ))}
+            </Slider>
+          )}
+        </SliderWrap>
       </Col>
     </Row>
   );
 };
 
-// 커뮤니티 게시글
-const CommunutyListItem = (props: { data: IRowData }) => {
-  const data = props.data;
-
-  return (
-    <Wrap key={data.field_id}>
-      <ThumbImg>
-        <img src={data.img_url} alt="썸네일 이미지" />
-      </ThumbImg>
-      <InfoWrap>
-        <Date>10월 22일 일요일</Date>
-        <FieldName>{data.field_name}</FieldName>
-        <Addr>
-          {data.area} {data.addr}
-          <a href="#">주소 복사</a>
-          <a href="#">지도 보기</a>
-        </Addr>
-        <Interest>
-          {data.views}
-          {data.like_cnt}
-        </Interest>
-        <Contour />
-        <UseInfo>
-          <Price>{data.price}원</Price>
-          <Hours>/ {data.hours}시간</Hours>
-        </UseInfo>
-      </InfoWrap>
-    </Wrap>
-  );
-};
-
 export default FieldList;
 
-const CommunityItemWrap = styled.div`
+const FieldItemWrap = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
-`;
 
-const Wrap = styled.div`
-  width: calc(50% - 1rem);
-  border-radius: 4px;
-  overflow: hidden;
-  ointer-events: none;
-  outline: 1px solid rgba(23, 23, 23, 0.08);
-  border-radius: 12px;
-  outline-offset: -1px;
-  margin-bottom: 1.2rem;
-`;
-
-const ThumbImg = styled.div`
-  position: relative;
-  overflow: hidden;
-  border-radius: 12px 12px 0 0;
-  width: 100%;
-  height: 246px;
-
-  & img {
-    width: 100%;
-    min-height: 196px;
-    height: 100%;
-    max-height: 100%;
+  & > div {
+    width: calc(50% - 1rem);
   }
 `;
 
-const InfoWrap = styled.div`
-  padding: 1rem;
-  text-align: left;
-`;
+const FieldItemMoreBtn = styled.button`
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  padding: 0;
+  overflow: visible;
+  cursor: pointer;
 
-const Date = styled.div`
-  font-size: 1.1rem;
-`;
-
-const FieldName = styled.div`
-  font-size: 1.3rem;
-  padding: 0.4rem 0;
-`;
-
-const Addr = styled.div`
-  color: #9c9c9c;
-
-  & a {
-    margin: 0 0.4rem;
+  width: 20%;
+  height: 36px;
+  min-width: 48px;
+  min-height: 32px;
+  font-size: 13px;
+  font-weight: 600;
+  border-radius: 6px;
+  padding: 7px 14px;
+  margin: 7px 0;
+  box-shadow: inset 0 0 0 1px #e1e2e4;
+  &:hover {
+    box-sizing: border-box;
+    transition: all 0.5s ease;
+    background-color: #f0f0f0;
+    color: #a2a1a1;
   }
 `;
 
-const Interest = styled.div`
-  color: #9c9c9c;
-`;
-
-const Contour = styled.div`
-  margin: 1rem 0;
-  width: 100%;
-  height: 1px;
-  background-color: rgba(23, 23, 23, 0.08);
-`;
-
-const UseInfo = styled.div`
+const SlideTitle = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: flex-start;
-`;
-
-const Price = styled.div`
-  font-size: 1.1rem;
+  justify-content: space-between;
+  font-size: 1.3rem;
   font-weight: 500;
+  margin: 1.5rem 0;
 `;
 
-const Hours = styled.div`
-  margin-left: 0.2rem;
-  color: rgb(157 155 155);
+const MoreBtn = styled.button`
+  font-size: 0.8rem;
+  &:hover {
+    color: #a2a1a1;
+    transition: all 0.5s ease;
+  }
+`;
+
+const SliderWrap = styled.div`
+  .slick-prev {
+    left: -57px;
+  }
+
+  .slick-next {
+    right: -37px;
+  }
+
+  .slick-next:before {
+    content: '→';
+  }
+
+  .slick-track {
+    margin: 0;
+    overflow: hidden;
+  }
+
+  .slick-prev:before,
+  .slick-next:before {
+    color: #c2c2c2;
+    font-size: 30px;
+    opacity: 1;
+  }
+
+  .slick-slide {
+    text-align: center;
+    margin-right: 1rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+
+    .slick-arrow {
+      right: 0;
+    }
+
+    .slick-prev:before,
+    .slick-next:before {
+      display: none;
+    }
+  }
 `;
