f042ee5305464828b8b718f5e0931cd36e8bce24
hagaaDev <tjsgml951012@gmail.com>
2023-10-31 16:44:35 +0900

    feat : 카카오 지도 x,y좌표값 type정의

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -4,11 +4,16 @@ import { styled } from 'styled-components';
 const { kakao } = window;
 /* 스크립트로 kakao maps api를 심어서 가져오면 window 전역 객체에 들어가는데 함수형 컴포넌트에서는 바로 인식 못함 */
 
-const KakaoMap = () => {
+interface IMapData {
+  mapX: number;
+  mapY: number;
+}
+
+const KakaoMap = ({ mapX, mapY }: IMapData) => {
   useEffect(() => {
     const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표,
+      center: new kakao.maps.LatLng(mapX, mapY), // 지도의 중심좌표,
       lever: 3,
     };
     const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
--- src/pages/FieldDetailPage.tsx
+++ src/pages/FieldDetailPage.tsx
@@ -63,7 +63,6 @@ const FieldDetailPage = () => {
   const [liked, setLiked] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [messageApi, contextHolder] = message.useMessage();
 
   /* 데이터 조회 */
   useEffect(() => {
@@ -103,56 +102,6 @@ const FieldDetailPage = () => {
   const putLiked = () => {
     setLiked(!liked);
   };
-
-  /* 카카오 맵 지도 */
-  /* useEffect(() => {
-    let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    let options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(37.586272, 127.029005), //지도의 중심좌표
-      level: 1, //지도의 레벨(확대, 축소 정도)
-    };
-    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    //---> 기본 맵 container, options, map 설정.
-
-    for (let i = 0; i < dummy.data.length; i++) {
-      displayMarker(dummy.data[i], i);
-    }
-
-    function displayMarker<
-      T extends {
-        name: string;
-        location_y: number;
-        location_x: number;
-        active: boolean;
-        point: number;
-      },
-    >(data: T, i: number) {
-      // 인포윈도우 표시될 위치(좌표)
-      let iwPosition = new window.kakao.maps.LatLng(
-        data.location_y,
-        data.location_x,
-      );
-
-      // 인포윈도우에 표출될 내용. HTML 문자열이나 document element 등이 가능하다.
-      var inactiveInfoWindow = `<div class="inactive infowindow""><span>${data.name}</span></div>`;
-
-      //인포윈도우
-      let infowindow;
-
-      infowindow = new window.kakao.maps.InfoWindow({
-        zIndex: 1,
-        position: iwPosition,
-        content: inactiveInfoWindow,
-        disableAutoPan: false,
-        map: map, //map에 해당 인포윈도우를 적용한다.
-      });
-    }
-
-    //중심좌표 재설정
-    var position = new window.kakao.maps.LatLng(37.586272, 127.029005);
-    map.setCenter(position);
-  }); */
 
   /* 모달 상태 관련 이벤트 핸들러 */
   const showModal = () => {
@@ -246,7 +195,7 @@ const FieldDetailPage = () => {
       <SectionWrap>
         <SectionHeader>
           <Title level={4}>지도</Title>
-          <KakaoMap />
+          <KakaoMap mapX={33.450701} mapY={126.570667} />
         </SectionHeader>
       </SectionWrap>
       <SectionWrap>
