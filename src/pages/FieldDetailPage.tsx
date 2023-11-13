9bfb886f1df3c70e7aedbcbffb770bfa2fcf318b
sunhee <tjsgml951012@gmail.com>
2023-11-14 00:08:07 +0900

    feat : 구장 상세정보 조회회 api

--- src/pages/FieldDetailPage.tsx
+++ src/pages/FieldDetailPage.tsx
@@ -14,35 +14,44 @@ import { Button, Typography, message } from 'antd';
 import { RedColor } from '../styles/CommonStyle';
 import FieldResvModal from '../components/field/FieldResvModal';
 import KakaoMap from '../components/common/KakaoMap';
+import { getFieldDetail } from '../service/FieldApi';
 
 interface IFieldItem {
-  field_id: string;
-  field_name: string;
-  area: string;
   addr: string;
-  opening_hours: string;
-  closing_hours: string;
-  price: number;
+  area: string | null;
+  closingHours: Date;
+  fieldId: string;
+  fieldNm: string;
+  fieldTp: string | null;
   hours: number;
+  insertDatetime: Date;
+  lat: string;
+  reviewCnt: number | undefined | null;
+  likeCnt: number;
+  likeYn: string;
+  lng: string;
   note: string;
-  swrm_co: string;
-  light: string;
-  views: number;
-  like_cnt: number;
-  img_url: string;
-  create_date: any;
+  openingHours: Date;
+  parkingTp: string;
+  price: number;
+  remarkTxt: string | null;
+  rentalSup: string | null;
+  size: string;
+  swrmYn: string | null;
+  useYn: string;
 }
 
 interface IRowData {
-  field_id: string;
-  field_name: string;
+  fieldId: string;
+  fieldNm: string;
   area: string;
   addr: string;
   price: number;
   hours: number;
-  views: number;
-  like_cnt: number;
-  img_url: string;
+  reviewCnt?: number | undefined;
+  likeCnt?: number | undefined;
+  imgUrl?: string | undefined;
+  likeYn?: string | undefined;
 }
 
 declare global {
@@ -56,10 +65,10 @@ const { Title, Text } = Typography;
 const FieldDetailPage = () => {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const field_id = searchParams.get('id');
+  const fieldId = searchParams.get('id');
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [data, setData] = useState<IRowData[]>([]);
+  const [data, setData] = useState<IFieldItem>();
   const [liked, setLiked] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
@@ -71,32 +80,17 @@ const FieldDetailPage = () => {
   }, [searchParams]);
 
   /* 전체로 조회 */
-  const getDataAll = () => {
-    Axios.get<IFieldItem[]>('/json/field.json').then((response) => {
-      const data = response.data;
-
-      if (data) {
-        const rows: IRowData[] = [];
-        data.forEach((d: IFieldItem) => {
-          const row = {
-            field_id: d.field_id,
-            field_name: d.field_name,
-            area: d.area,
-            addr: d.addr,
-            price: d.price,
-            hours: d.hours,
-            views: d.views,
-            like_cnt: d.like_cnt,
-            img_url: d.img_url,
-          };
-          rows.push(row);
-        });
-
-        setData(rows);
-        setIsLoading(false);
-      }
-    });
+  const getDataAll = async () => {
+    if (fieldId !== null) {
+      const d: any = await getFieldDetail(fieldId);
+      setData(d.data.result[0]);
+      setIsLoading(false);
+    }
   };
+
+  useEffect(() => {
+    console.log('data', data);
+  }, [data]);
 
   /* 좋아요 기능 */
   const putLiked = () => {
@@ -126,116 +120,119 @@ const FieldDetailPage = () => {
     navigator.clipboard.writeText(text);
     message.info(`주소를 복사하였습니다!`);
   };
+
   return (
     <Wrap>
-      <BackgroundImg>
-        <ActionWrap>
-          <WishBtn onClick={() => putLiked()}>
-            <Heart color={liked ? RedColor : '#696969'} />
-          </WishBtn>
-        </ActionWrap>
-      </BackgroundImg>
-      <SectionWrap>
-        <Interest>
-          <MessageSquare />
-          12
-          <Heart />1
-        </Interest>
-        <Title level={2}>구장이름</Title>
-        <FlexWrap type="secondary">
-          경기도 부천시 주소주소
-          <Button
-            type="link"
-            onClick={() => handleCopyclipBoard('주소를 복사했습니다')}
-          >
-            주소복사
-          </Button>
-        </FlexWrap>
-        <Contour />
-        <FlexWrap>
-          <div>
-            <Price>10,000원</Price>
-            <Hours>/ 2시간</Hours>
-          </div>
-          <Button type="primary" size="large" onClick={showModal}>
-            예약신청
-          </Button>
-        </FlexWrap>
-      </SectionWrap>
-      <SectionWrap>
-        <SectionHeader>
-          <Title level={4}>경기장 정보</Title>
-          <FieldInfo>
-            <li>
-              <Flag />
-              40x20m
-            </li>
-            <li>
-              <ShowerHead />
-              샤워실
-            </li>
-            <li>
-              <CarFront />
-              무료주차
-            </li>
-            <li>
-              <Footprints />
-              풋살화 대여
-            </li>
-          </FieldInfo>
-          <Contour />
-          <Title level={5}>구장 특이사항</Title>
-          <Contents>
-            ■주차: 무료 ■흡연: 흡연구역 외 절대 금연(흡연구역 외에서 흡연 적발
-            시 이후 서비스 이용에 제재가 있을 수 있음을 안내합니다.) ■풋살화
-            대여: O (사이즈: 220~290mm / 대여료: 3천 원)
-          </Contents>
-        </SectionHeader>
-      </SectionWrap>
-      <SectionWrap>
-        <SectionHeader>
-          <Title level={4}>지도</Title>
-          <KakaoMap mapX={33.450701} mapY={126.570667} />
-        </SectionHeader>
-      </SectionWrap>
-      <SectionWrap>
-        <SectionHeader>
-          <Title level={4}>환불정책</Title>
-        </SectionHeader>
-        <Title level={5}>신청 취소 시 환불 기준</Title>
-        <Contents>
-          예약신청일 ~ 매치 3일 전 : 무료 취소 <br />
-          매치 2일 전 : 80% 환급 <br />
-          매치 1일 전 : 환불 불가
-          <br />
-        </Contents>
-        <Title level={5}>그 외 취소 기준</Title>
-        <Contents>
-          결제 후 30분 이내에는 하루 1회에 한해 무료 취소가 가능합니다. (단,
-          매치 시작 90분 이내일 경우 불가) 쿠폰 신청자는 매치 시작 90분 전까지
-          취소 시 쿠폰이 반환됩니다. 결제 시 실 결제금액(쿠폰 제외)을 기준으로
-          위 규정에 따라 환급됩니다. 매치 시작 90분 전까지 최소 인원이 모이지
-          않을 시 카카오톡 혹은 LMS으로 안내되며, 자동 전액 환불됩니다. (단,
-          공지 전 직접 취소하시는 경우 상단 일반 환불 규정대로 처리되니
-          유의하시길 바랍니다)
-          <br />
-        </Contents>
-        <Title level={5}>다음의 경우는 환불이 불가합니다.</Title>
-        <Contents>
-          구장, 날짜, 시간 등을 실수로 잘못 선택한 경우 부상, 취업 등 개인
-          사정으로 신청된 매치에 참여하지 못하는 경우 단체 혹은 지인과의 참가로
-          매치 취소 혹은 변경을 원하는 경우 황사/미세먼지로 인해 취소 혹은
-          변경을 요청하는 경우 단순 변심으로 취소 혹은 변경을 요청하는 경우
-          <br />
-        </Contents>
-      </SectionWrap>
-      <FieldResvModal
-        fieldName="구장이름"
-        isModalOpen={isModalOpen}
-        handleOk={handleOk}
-        handleCancel={handleCancel}
-        confirmLoading={confirmLoading}
-      />
+      {!isLoading && (
+        <>
+          <BackgroundImg>
+            <ActionWrap>
+              <WishBtn onClick={() => putLiked()}>
+                <Heart color={data?.likeYn === '0' ? RedColor : '#696969'} />
+              </WishBtn>
+            </ActionWrap>
+          </BackgroundImg>
+          <SectionWrap>
+            <Interest>
+              <MessageSquare />
+              {data?.reviewCnt}
+              <Heart />
+              {data?.likeCnt}
+            </Interest>
+            <Title level={2}>{data?.fieldNm}</Title>
+            <FlexWrap type="secondary">
+              {data?.addr}
+              <Button
+                type="link"
+                onClick={() => handleCopyclipBoard('주소를 복사했습니다')}
+              >
+                주소복사
+              </Button>
+            </FlexWrap>
+            <Contour />
+            <FlexWrap>
+              <div>
+                <Price>{data?.price}원</Price>
+                <Hours>/ {data?.hours}시간</Hours>
+              </div>
+              <Button type="primary" size="large" onClick={showModal}>
+                예약신청
+              </Button>
+            </FlexWrap>
+          </SectionWrap>
+          <SectionWrap>
+            <SectionHeader>
+              <Title level={4}>경기장 정보</Title>
+              <FieldInfo>
+                <li>
+                  <Flag />
+                  {data?.size}m
+                </li>
+                <li>
+                  <ShowerHead />
+                  {data?.swrmYn === '1' ? '샤워실' : ''}
+                </li>
+                <li>
+                  <CarFront />
+                  {data?.parkingTp === '0' ? '무료주차' : '유료주차'}"
+                </li>
+                <li>
+                  <Footprints />
+                  {data?.rentalSup}
+                </li>
+              </FieldInfo>
+              <Contour />
+              <Title level={5}>구장 특이사항</Title>
+              <Contents>{data?.note}</Contents>
+            </SectionHeader>
+          </SectionWrap>
+          <SectionWrap>
+            <SectionHeader>
+              <Title level={4}>지도</Title>
+              {/* <KakaoMap mapX={data?.lat} mapY={data?.lng} /> */}
+            </SectionHeader>
+          </SectionWrap>
+          <SectionWrap>
+            <SectionHeader>
+              <Title level={4}>환불정책</Title>
+            </SectionHeader>
+            <Title level={5}>신청 취소 시 환불 기준</Title>
+            <Contents>
+              예약신청일 ~ 매치 3일 전 : 무료 취소 <br />
+              매치 2일 전 : 80% 환급 <br />
+              매치 1일 전 : 환불 불가
+              <br />
+            </Contents>
+            <Title level={5}>그 외 취소 기준</Title>
+            <Contents>
+              결제 후 30분 이내에는 하루 1회에 한해 무료 취소가 가능합니다. (단,
+              매치 시작 90분 이내일 경우 불가) 쿠폰 신청자는 매치 시작 90분
+              전까지 취소 시 쿠폰이 반환됩니다. 결제 시 실 결제금액(쿠폰 제외)을
+              기준으로 위 규정에 따라 환급됩니다. 매치 시작 90분 전까지 최소
+              인원이 모이지 않을 시 카카오톡 혹은 LMS으로 안내되며, 자동 전액
+              환불됩니다. (단, 공지 전 직접 취소하시는 경우 상단 일반 환불
+              규정대로 처리되니 유의하시길 바랍니다)
+              <br />
+            </Contents>
+            <Title level={5}>다음의 경우는 환불이 불가합니다.</Title>
+            <Contents>
+              구장, 날짜, 시간 등을 실수로 잘못 선택한 경우 부상, 취업 등 개인
+              사정으로 신청된 매치에 참여하지 못하는 경우 단체 혹은 지인과의
+              참가로 매치 취소 혹은 변경을 원하는 경우 황사/미세먼지로 인해 취소
+              혹은 변경을 요청하는 경우 단순 변심으로 취소 혹은 변경을 요청하는
+              경우
+              <br />
+            </Contents>
+          </SectionWrap>
+          <FieldResvModal
+            fieldName="구장이름"
+            isModalOpen={isModalOpen}
+            handleOk={handleOk}
+            handleCancel={handleCancel}
+            confirmLoading={confirmLoading}
+          />
+        </>
+      )}
     </Wrap>
   );
 };
