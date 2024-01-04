327481d803f45d01244857a91bd0d87765571be2
sunhee <tjsgml951012@gmail.com>
2024-01-04 20:24:57 +0900

    새로고침 시 redux 상태관리 유지

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/Global';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import { store } from './stores/Store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import store from './stores/Store';
+
+export let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
@@ -15,13 +20,12 @@ root.render(
     <BrowserRouter>
       <GlobalStyle></GlobalStyle>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/stores/Store.tsx
+++ src/stores/Store.tsx
@@ -1,17 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import { MenuSlice } from './features/MypageMenuSlice';
 import { getReserDataSlice } from './features/GetReservationSlice';
 import { authenticationSlice } from './features/AuthenticationSlice';
-export const store = configureStore({
-  reducer: {
-    menu: MenuSlice.reducer,
-    getReserData: getReserDataSlice.reducer,
-    user: authenticationSlice.reducer,
-  },
+
+const reducers = combineReducers({
+  menu: MenuSlice.reducer,
+  getReserData: getReserDataSlice.reducer,
+  user: authenticationSlice.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<
   ReturnType<typeof store.getState>
 > = useSelector;
+
+export default store;
